35c809e7e78f77abc06dcff8c33e49ace9a13b3a
Mikhail <misha.shabalin@mail.ru>
2023-05-24 19:56:39 +0300

    Add Tag Class iin models.py

--- articles/models.py
+++ articles/models.py
@@ -6,7 +6,8 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True,
+                              verbose_name='Изображение',)
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +15,20 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=10, verbose_name='Раздел')
+    article = models.ManyToManyField(Article,
+                                     through='TagScope',
+                                     related_name='tags',
+                                     verbose_name='Статьи',
+                                     )
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
