98ab2847f5711c7f1cce172fc61912891cdf222e
Mikhail <misha.shabalin@mail.ru>
2023-05-24 20:00:59 +0300

    Add Scope Class in models.py

--- articles/models.py
+++ articles/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 class Article(models.Model):
 
-    title = models.CharField(max_length=256, verbose_name='Название')
+    title = models.CharField(max_length=256,
+                             verbose_name='Название'
+                             )
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True,
-                              verbose_name='Изображение',)
+    image = models.ImageField(null=True,
+                              blank=True,
+                              verbose_name='Изображение',
+                              )
 
     class Meta:
         verbose_name = 'Статья'
@@ -18,7 +22,10 @@ class Article(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=10, verbose_name='Раздел')
+
+    name = models.CharField(max_length=10,
+                            verbose_name='Раздел'
+                            )
     article = models.ManyToManyField(Article,
                                      through='TagScope',
                                      related_name='tags',
@@ -32,3 +39,19 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
+class Scope(models.Model):
+
+    article = models.ForeignKey(Article,
+                                on_delete=models.CASCADE,
+                                related_name='scopes',
+                                verbose_name='Статьи',
+                                )
+
+    tag = models.ForeignKey(Tag,
+                            on_delete=models.CASCADE,
+                            related_name='scopes',
+                            verbose_name='Разделы',
+                            )
+
+    is_main = models.BooleanField(verbose_name='Основной',)
