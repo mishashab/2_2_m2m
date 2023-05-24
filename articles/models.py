a27cd8844abdd987833297975466d266c7c48313
Mikhail <misha.shabalin@mail.ru>
2023-05-24 20:12:06 +0300

    fixed2 models.py

--- articles/models.py
+++ articles/models.py
@@ -16,6 +16,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
