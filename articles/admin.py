7390db7e1008595627716a3a96f6161c74b1aa87
Mikhail <misha.shabalin@mail.ru>
2023-05-24 20:43:28 +0300

    ready admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -13,18 +13,22 @@ class TagAdmin(admin.ModelAdmin):
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
         tags = []
-        main = []
+        main_tags = []
         for form in self.forms:
             if len(form.cleaned_data):
                 tags.append(form.cleaned_data['tag'])
-                main.append(1 if form.cleaned_data['is_main'] else 0)
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-            # form.cleaned_data
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            # raise ValidationError('Тут всегда ошибка')
+                main_tags.append(1 if form.cleaned_data['is_main'] else 0)
+
+            if len(tags) != len(set(tags)):
+                raise ValidationError('Один из тегов повторяется')
+
+            if sum(main_tags) == 0:
+                raise ValidationError('Статье не присвоен основной тег')
+
+            if sum(main_tags) > 1:
+                raise ValidationError('В статье главный основной тег '
+                                      'используется более 1 раза')
+
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
